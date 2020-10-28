2762d05513998b622ad7565421b9c376d61101e3
Matt Logan <mlogan5212@gmail.com>
2020-10-27 19:05:35 -0500

    fixed error in commit

--- animals/cobra.py
+++ animals/cobra.py
@@ -5,5 +5,4 @@ from movements import Slithering
 class Cobra(Critter, Slithering):
     def __init__(self, name, species, food, chipnum):
         super().__init__(name, species, food, chipnum)
-        self.slithering = True
         Slithering.__init__(self)
